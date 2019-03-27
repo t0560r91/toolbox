3a8100603b46f7211171af0baf569b4809e333fb
Seho Kim <stonechild88@gmail.com>
2019-03-27 10:42:22 -0700

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import re, os
+
+
+def find_packages(path='.'):
+    ret = []
+    for root, dirs, files in os.walk(path):
+        if '__init__.py' in files:
+            ret.append(re.sub('^[^A-z0-9_]+', '', root.replace('/', '.')))
+    return ret
+
+
 setup(name='toolbox',
       description='machine learning tools',
       version='0.0.1',
       author='Seho Kim',
       url='https://github.com/sehokim88/customtools',
-      package_dir = {
-            'toolbox': 'toolbox',
-            'toolbox.pipelining': 'toolbox/pipelining'},
-      packages=['toolbox', 'toolbox.pipelining'],
+      packages=find_packages(),
       install_requires=['numpy', 'pandas']
 )
