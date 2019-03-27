10fe088d5cc83c139a7c6c95fe080df90f204c0c
Seho Kim <stonechild88@gmail.com>
2019-03-27 09:14:10 -0700

    re

---
+++ toolbox/pipelining.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+class Pipeline:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+
+    def transform(self, X):
+        pass
