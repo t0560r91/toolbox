bd4b354fbb423cb4802b08497dfe52704d4461f8
Seho Kim <34978266+sehokim88@users.noreply.github.com>
2019-03-27 07:31:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-
+from setuptools import setup
+setup(name='simplefunc',
+      description='Functions that simplify repetitive and tedious procedures',
+      version='0.0.1',
+      author='Seho Kim',
+      url='https://github.com/sehokim88/customtools',
+      packages=['toolbox'],
+      install_requires=['numpy', 'pandas']
+)
