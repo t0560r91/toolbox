edc2fafaa06c2b30443c2e742fcd516aa8087ef9
Seho Kim <34978266+sehokim88@users.noreply.github.com>
2019-03-27 07:46:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='toolbox',
       version='0.0.1',
       author='Seho Kim',
       url='https://github.com/sehokim88/customtools',
-      packages=['toolbox'],
+      packages=['toolbox', 'preprocessing', 'pipelining', 'modeling', 'scoring'],
       install_requires=['numpy', 'pandas']
 )
