ddd149451bc78b69ddc329465168429001857c4c
Seho Kim <34978266+sehokim88@users.noreply.github.com>
2019-03-27 07:44:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-setup(name='simplefunc',
-      description='Functions that simplify repetitive and tedious procedures',
+setup(name='toolbox',
+      description='machine learning tools',
       version='0.0.1',
       author='Seho Kim',
       url='https://github.com/sehokim88/customtools',
